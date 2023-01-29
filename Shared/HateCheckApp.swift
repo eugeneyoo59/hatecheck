858104e07c8426c504c69904598c44fab7e29236
eugeneyoo59 <63625721+eugeneyoo59@users.noreply.github.com>
2023-01-29 14:43:33 +0900

    Add files via upload

---
+++ Shared/HateCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  HateCheckApp.swift
+//  Shared
+//
+//  Created by Eugene Yoo on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct HateCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
